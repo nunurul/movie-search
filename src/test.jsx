721614a68a41aa797780fa11125f80e7a9c53aa8
nunurul <noe.rul.khoir@gmail.com>
2024-08-20 08:41:08 +0700

    second commit movie

--- src/test.jsx
+++ src/test.jsx
@@ -1,36 +1,17 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import profile from "./img/profile.png";
+import img from "./img/profile.png";
 import axios from "axios";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.themoviedb.org/3/trending/movie/day",
-          {
-            params: {
-              api_key: "c0895a05ec9be10cbd7ee66540f70006",
-            },
-          }
-        );
-        setMovies(response.data.results);
-      } catch (error) {
-        console.error(`Terjadi error pada menampilkan gambar`, error);
-      }
-    };
-    fetchMovies();
-  }, []);
-
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  const handleSearchButton = async () => {
+  const handleSearchSubmit = async () => {
     const response = await axios.get(
       "https://api.themoviedb.org/3/search/movie",
       {
@@ -45,19 +26,39 @@ function App() {
 
   const handleSearchKeyDown = (event) => {
     if (event.key === "Enter") {
-      handleSearchButton();
+      handleSearchSubmit();
     }
   };
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.themoviedb.org/3/trending/movie/day",
+          {
+            params: {
+              api_key: "c0895a05ec9be10cbd7ee66540f70006",
+            },
+          }
+        );
+        setMovies(response.data.results);
+      } catch (error) {
+        console.error(`Terjadi kesalahan pada tampilan`, error);
+      }
+    };
+    fetchTrendingMovies();
+  }, []);
 
   return (
     <div className="container">
       <div className="header">
-        <h1 className="logo">NunuMovie</h1>
-        <div className="box-search">
-          <i className="bx bx-search"></i>
+        <h1 className="logo-text">NunuMovie</h1>
+        <div className="search-bar">
+          <i className="bx bx-search search-icon"></i>
           <input
             type="text"
-            className="search-input"
+            className="search"
+            id="search"
             placeholder="Search Movie"
             value={searchQuery}
             onChange={handleSearchChange}
@@ -67,7 +68,7 @@ function App() {
         <div className="box-profile">
           <i className="bx bxs-cog"></i>
           <i className="bx bxs-bell"></i>
-          <img src={profile} alt="profile_image" className="img-profile" />
+          <img src={img} alt="" className="img-profile" />
         </div>
       </div>
       <div className="main">
@@ -78,35 +79,33 @@ function App() {
               alt={movie.title}
               className="bg-movie"
             />
-            <div className="content-movie">
+            <div className="content">
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
-                className="poster-movie"
+                className="detail-img"
               />
-              <div className="box-title">
-                <h2 className="title">
-                  {movie.title}{" "}
-                  <span className="year">
-                    (
-                    {movie.release_date
-                      ? movie.release_date.split("-")[0]
-                      : "Unknown"}
-                    )
-                  </span>
-                </h2>
-              </div>
+              <p className="title">
+                {movie.title}{" "}
+                <span className="year">
+                  (
+                  {movie.release_date
+                    ? movie.release_date.split("-")[0]
+                    : "Unknown"}
+                  )
+                </span>
+              </p>
               <div className="box-star">
                 <i className="bx bxs-star"></i>
-                <div className="rate-star">
-                  <p className="value">{movie.vote_average.toFixed(1)}</p>
+                <div className="text-star">
+                  <p className="number-star">{movie.vote_average.toFixed(1)}</p>
                   <span>/</span>
-                  <p className="value">10</p>
+                  <p className="number-star">10</p>
                 </div>
               </div>
               <p className="desc">
                 {movie.overview.length > 150
-                  ? `${movie.overview.substring(0, 150)}...`
+                  ? `${movie.overview.substring(0, 150)}`
                   : movie.overview}
               </p>
             </div>
